d1cb9d50e337be420aa00c207b707d55cdfa98ec
Tufaarf <daffa.tfail@gmail.com>
2025-09-26 15:21:56 +0700

    add hasfactory on legalitas model

--- app/Models/legalitas.php
+++ app/Models/legalitas.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class legalitas extends Model
 {
+       use HasFactory;
+
     protected $table = 'legalitas';
     protected $fillable = ['nama_legalitas', 'image'];
 }
